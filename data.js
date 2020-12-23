68ee991dc620292a5bc267e6061e1e0868d9b3bf
Juan Lee <juanlee@kaist.ac.kr>
2020-12-23 20:56:15 +0900

    Add next-bus function - Add next bus time ui - Add remaining time ui - Update the UI every one second

--- data.js
+++ data.js
@@ -1,3 +1,7 @@
+/*
+    DATA
+*/
+
 var redStations = [
     "PED/walkthru gate",
     "AV/CPX Gate",
@@ -39,6 +43,88 @@ var redStations = [
     "PED/walkthru gate",
 ];
 
+var redStartingsWeekday = [
+    "0530",
+    "0534",
+    "0536",
+    "0537",
+    "0539",
+    "0541",
+    "0547",
+    "0549",
+    "0550",
+    "0553",
+    "0554",
+    "0555",
+    "0557",
+    "0558",
+    "0600",
+    "0602",
+    "0604",
+    "0606",
+    "0609",
+    "0610",
+    "0611",
+    "0613",
+    "0614",
+    "0615",
+    "0616",
+    "0619",
+    "0620",
+    "0621",
+    "0623",
+    "0625",
+    "0626",
+    "0628",
+    "0632",
+    "0634",
+    "0636",
+    "0638",
+    "0640",
+    "0641",
+];
+
+var redStartingsWeekend = [
+    "0730",
+    "0734",
+    "0736",
+    "0737",
+    "0739",
+    "0741",
+    "0747",
+    "0749",
+    "0750",
+    "0753",
+    "0754",
+    "0755",
+    "0757",
+    "0758",
+    "0800",
+    "0802",
+    "0804",
+    "0806",
+    "0809",
+    "0810",
+    "0811",
+    "0813",
+    "0814",
+    "0815",
+    "0816",
+    "0819",
+    "0820",
+    "0821",
+    "0823",
+    "0825",
+    "0826",
+    "0828",
+    "0832",
+    "0834",
+    "0836",
+    "0838",
+    "0840",
+    "0841",
+];
+
 var greenStations = [
     "Bus Terminal",
     "Medical Clinic",
@@ -74,6 +160,80 @@ var greenStations = [
     "Walkthru gate",
     "CPX",
     "Bus Terminal",
+];
+
+var greenStartingsWeekday = [
+    "0530",
+    "0533",
+    "0534",
+    "0536",
+    "0538",
+    "0539",
+    "0541",
+    "0543",
+    "0545",
+    "0546",
+    "0548",
+    "0550",
+    "0552",
+    "0554",
+    "0556",
+    "0558",
+    "0600",
+    "0601",
+    "0603",
+    "0605",
+    "0607",
+    "0608",
+    "0610",
+    "0612",
+    "0614",
+    "0616",
+    "0617",
+    "0621",
+    "0622",
+    "0624",
+    "0626",
+    "0627",
+    "0631",
+    "0632",
+];
+
+var greenStartingsWeekend = [
+    "0730",
+    "0733",
+    "0734",
+    "0736",
+    "0738",
+    "0739",
+    "0741",
+    "0743",
+    "0745",
+    "0746",
+    "0748",
+    "0750",
+    "0752",
+    "0754",
+    "0756",
+    "0758",
+    "0800",
+    "0801",
+    "0803",
+    "0805",
+    "0807",
+    "0808",
+    "0810",
+    "0812",
+    "0814",
+    "0816",
+    "0817",
+    "0821",
+    "0822",
+    "0824",
+    "0826",
+    "0827",
+    "0831",
+    "0832",
 ];
 
 var blueStations = [
@@ -116,3 +276,201 @@ var blueStations = [
     "AV/CPX Gate",
     "PED/walkthru gate",
 ];
+
+var blueStartingsWeekday = [
+    "0540",
+    "0541",
+    "0543",
+    "0546",
+    "0548",
+    "0550",
+    "0554",
+    "0555",
+    "0556",
+    "0557",
+    "0559",
+    "0600",
+    "0602",
+    "0605",
+    "0606",
+    "0607",
+    "0609",
+    "0611",
+    "0612",
+    "0613",
+    "0615",
+    "0617",
+    "0619",
+    "0622",
+    "0624",
+    "0625",
+    "0627",
+    "0628",
+    "0629",
+    "0630",
+    "0631",
+    "0637",
+    "0639",
+    "0642",
+    "0643",
+    "0645",
+    "0647",
+    "0651",
+];
+
+var blueStartingsWeekend = [
+    "0745",
+    "0746",
+    "0748",
+    "0751",
+    "0753",
+    "0755",
+    "0759",
+    "0800",
+    "0801",
+    "0802",
+    "0804",
+    "0805",
+    "0807",
+    "0810",
+    "0811",
+    "0812",
+    "0814",
+    "0816",
+    "0817",
+    "0818",
+    "0820",
+    "0822",
+    "0824",
+    "0827",
+    "0829",
+    "0830",
+    "0832",
+    "0833",
+    "0834",
+    "0835",
+    "0836",
+    "0842",
+    "0844",
+    "0847",
+    "0848",
+    "0850",
+    "0852",
+    "0856",
+];
+
+/*
+    DATA Parsers
+*/
+var addQuater = (time) => {
+    time += 15;
+    if (time % 100 >= 60) {
+        time = (parseInt(time / 100) + 1) * 100 + (time % 100) - 60;
+    }
+    return time;
+};
+
+var subQuater = (time) => {
+    time -= 15;
+    if (time < 0) time += 2400;
+    if (time % 100 > 60) {
+        time -= 40;
+    }
+    return time;
+};
+
+var getNextRedBus = (stationIdx, now, day, isTomorrowHoliday) => {
+    // get starting time and compare with it
+    var starting = 0;
+    if (day >= 1 && day <= 5) {
+        starting = redStartingsWeekday[stationIdx];
+    } else {
+        starting = redStartingsWeekend[stationIdx];
+    }
+    starting = parseInt(starting);
+    if (starting > now) return starting;
+
+    // calculate ending time
+    var ending = 2400;
+    if (
+        (day === 4 && isTomorrowHoliday) ||
+        day === 5 ||
+        day === 6 ||
+        (day === 7 && isTomorrowHoliday)
+    ) {
+        ending = 2500;
+    }
+
+    // get next bus
+    var temp = addQuater(starting);
+    while (temp <= ending) {
+        if (now < temp) return temp;
+        temp = addQuater(temp);
+    }
+    return null; // no more bus today
+};
+
+var getNextGreenBus = (stationIdx, now, day) => {
+    // get starting time and compare with it
+    var starting = 0;
+    if (day >= 1 && day <= 5) {
+        starting = greenStartingsWeekday[stationIdx];
+    } else {
+        starting = greenStartingsWeekend[stationIdx];
+    }
+    starting = parseInt(starting);
+    if (starting > now) return starting;
+
+    // get next bus
+    var temp = addQuater(starting);
+    while (temp <= 2400) {
+        if (now < temp) return temp;
+        temp = addQuater(temp);
+    }
+    return null; // no more bus today
+};
+
+var getNextBlueBus = (stationIdx, now, day, isTomorrowHoliday) => {
+    // get starting time and compare with it
+    var starting = 0;
+    if (day >= 1 && day <= 5) {
+        starting = blueStartingsWeekday[stationIdx];
+    } else {
+        starting = blueStartingsWeekend[stationIdx];
+    }
+    starting = parseInt(starting);
+    if (starting > now) return starting;
+
+    // calculate ending time
+    var ending = 2400;
+    if (day >= 1 && day <= 5) {
+        if (day === 5 || (day === 4 && isTomorrowHoliday)) ending = 2440;
+        else ending = 2355;
+    } else {
+        if (day === 6 || isTomorrowHoliday) ending = 2445;
+        else ending = 2345;
+    }
+
+    // get next bus
+    var temp = addQuater(starting);
+    while (temp <= ending) {
+        if (now < temp) return temp;
+        temp = addQuater(temp);
+    }
+    return null; // no more bus today
+};
+
+var getNextBus = (bus, stationIdx, now, day, isTomorrowHoliday) => {
+    if (bus.toLowerCase() === "red") {
+        return getNextRedBus(stationIdx, parseInt(now), day, isTomorrowHoliday);
+    } else if (bus.toLowerCase() === "green") {
+        return getNextGreenBus(stationIdx, parseInt(now), day);
+    } else {
+        return getNextBlueBus(
+            stationIdx,
+            parseInt(now),
+            day,
+            isTomorrowHoliday
+        );
+    }
+};
--- script.js
+++ script.js
@@ -1,3 +1,21 @@
+var currentNextBusOnUI = 0;
+
+$(document).ready(function () {
+    var now = new Date();
+    var hr = now.getHours();
+    var mn = now.getMinutes();
+    var nextBus = getNextBus("red", 26, hr * 100 + mn, now.getDay(), false);
+    currentNextBusOnUI = nextBus;
+
+    if (nextBus < 1000) {
+        $("#next-bus").text(
+            `Next 0${parseInt(nextBus / 100)}:${nextBus % 100}`
+        );
+    } else {
+        $("#next-bus").text(`Next ${parseInt(nextBus / 100)}:${nextBus % 100}`);
+    }
+});
+
 /* Bus Selection */
 $(".menu a").click(function () {
     $(".menu a").removeClass("active");
@@ -36,9 +54,57 @@ $(".menu a").click(function () {
         $("#current .button").html(
             `<i class="bus icon"></i> ${$(this).text()}`
         );
+
+        var stationIdx = $(this).index();
+        var now = new Date();
+        var hr = now.getHours();
+        var mn = now.getMinutes();
+        var nextBus = getNextBus(
+            currentBus,
+            stationIdx,
+            hr * 100 + mn,
+            now.getDay(),
+            false
+        );
+        currentNextBusOnUI = nextBus;
+
+        if (nextBus < 1000) {
+            $("#next-bus").text(
+                `Next 0${parseInt(nextBus / 100)}:${nextBus % 100}`
+            );
+        } else {
+            $("#next-bus").text(
+                `Next ${parseInt(nextBus / 100)}:${nextBus % 100}`
+            );
+        }
     });
 
     $(".ui.sidebar")
         .sidebar("setting", "transition", "overlay")
         .sidebar("toggle");
 });
+
+/* Timer */
+setInterval(function () {
+    var now = new Date();
+    var hr = now.getHours();
+    var mn = now.getMinutes();
+    var sn = now.getSeconds();
+
+    var leftmn = (currentNextBusOnUI % 100) - mn - 1;
+    if (parseInt(currentNextBusOnUI / 100) > hr) {
+        leftmn += 60;
+    }
+    var leftsn = 60 - sn;
+
+    var smn = String(leftmn);
+    if (leftmn < 10) {
+        smn = "0" + smn;
+    }
+    var ssn = String(leftsn);
+    if (leftsn < 10) {
+        ssn = "0" + ssn;
+    }
+
+    $("#remaining-time").text(`Remaining ${smn}:${ssn}`);
+}, 1000);
