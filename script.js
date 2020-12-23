72adb5bf86627ee634657f1df0d42e7f779e2619
Juan Lee <juanlee@kaist.ac.kr>
2020-12-23 11:51:39 +0900

    Update UI - Add table - Add timer

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-/* BUS SELECTION */
+/* Bus Selection */
 $(".menu a").click(function () {
     $(".menu a").removeClass("active");
     $(this).addClass("active");
@@ -18,6 +18,26 @@ $(".menu a").click(function () {
             $(".ui.sidebar").append(`<a class="item">${station}</a>`);
         });
     }
+
+    /* Add station selection listener */
+    $(".sidebar .item").click(function () {
+        $(".ui.sidebar").sidebar("hide");
+
+        $("#current .button").removeClass("red");
+        $("#current .button").removeClass("green");
+        $("#current .button").removeClass("blue");
+        $("#current .button").addClass(currentBus.toLowerCase());
+
+        $("table").removeClass("red");
+        $("table").removeClass("green");
+        $("table").removeClass("blue");
+        $("table").addClass(currentBus.toLowerCase());
+
+        $("#current .button").html(
+            `<i class="bus icon"></i> ${$(this).text()}`
+        );
+    });
+
     $(".ui.sidebar")
         .sidebar("setting", "transition", "overlay")
         .sidebar("toggle");
